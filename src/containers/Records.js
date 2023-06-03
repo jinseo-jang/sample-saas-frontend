ff027ef547eecbd9e21a3fd04dd078f4a64de9cb
Jang <jjinseo@c889f3b72510.ant.amazon.com>
2023-06-03 14:11:27 +0900

    Improved records ui

--- src/containers/Records.js
+++ src/containers/Records.js
@@ -4,7 +4,7 @@ import wait from "waait";
 import Badge from "react-bootstrap/Badge";
 import ListGroup from "react-bootstrap/ListGroup";
 import "./Records.css";
-import Spinner from 'react-bootstrap/Spinner';
+import Spinner from "react-bootstrap/Spinner";
 import { useParams } from "react-router-dom";
 import { Auth } from "aws-amplify";
 import { fetchUserInformation } from "../authUtils";
@@ -15,13 +15,12 @@ export default function Records() {
   //     items: {}
   // });
 
-  let {userid} = useParams();
+  let { userid } = useParams();
 
   const [content, setContent] = useState([]);
   const [loading, setLoading] = useState(true);
   const [tenantId, setTenantId] = useState(null);
   const [userName, setUserName] = useState(null);
-
 
   useEffect(() => {
     async function fetchUserInfo() {
@@ -36,7 +35,7 @@ export default function Records() {
   }, []);
 
   const listRecords = async () => {
-    const apiUrl = "http://127.0.0.1:5000/api/records"
+    const apiUrl = "http://127.0.0.1:5000/api/records";
     // const data = { user_name: userName };
     const session = await Auth.currentSession();
     const idToken = session.getIdToken().getJwtToken();
@@ -64,30 +63,57 @@ export default function Records() {
 
   return (
     <div className="Records">
-      <div className="lander">
-        <h1 align="center">UserID: {userid}</h1>
-        <h3 align="center">Records of clock-in/out</h3>
+      <div className="lander mt-0 mb-0">
+        <div className="text-start">
+          <span className="fw-bold">Tenant ID:</span> {tenantId}
+        </div>
+        <div className="text-start">
+          <span className="fw-bold">User Name:</span> {userName}
+        </div>
       </div>
-      <div>
+      <div className="lists mt-0 mb-0">
         {loading ? (
           <Spinner animation="border" variant="primary" />
         ) : (
           content.map((record) => (
-            <ListGroup as="ol" key={record.record_id} horizontal={record.record_id}>
+            <ListGroup
+              as="ol"
+              key={record.record_id}
+              horizontal={record.record_id}
+            >
               <ListGroup.Item
                 as="li"
                 className="d-flex justify-content-between align-items-start"
               >
                 <div className="ms-2 me-auto">
-                  <div className="fw-bold">Record ID: {record.record_id}</div>
-                  <div>Tenant ID: {record.tenant_id}</div>
-                  <div>User Name: {record.user_name}</div>
-                  <div>Clock In: {record.clock_in}</div>
-                  <div>Clock Out: {record.clock_out || 'Not available'}</div>
+                  <div>
+                    <span className="fw-bold">Tenant ID:</span>{" "}
+                    {record.tenant_id}
+                  </div>
+                  <div>
+                    <span className="fw-bold">User Name:</span>{" "}
+                    {record.user_name}
+                  </div>
+                  <div>
+                    <span className="fw-bold text-primary">Clock In:</span>{" "}
+                    {record.clock_in || "Not yet"}
+                  </div>
+                  {record.clock_out && (
+                    <div>
+                      <span className="fw-bold text-secondary">Clock Out:</span>{" "}
+                      {record.clock_out}
+                    </div>
+                  )}
                 </div>
-                <Badge bg="primary" pill>
-                  clock-in
-                </Badge>
+                {record.clock_out ? (
+                  <Badge bg="secondary" pill>
+                    clock-out
+                  </Badge>
+                ) : record.clock_in ? (
+                  <Badge bg="primary" pill>
+                    clock-in
+                  </Badge>
+                ) : null}
               </ListGroup.Item>
             </ListGroup>
           ))
@@ -109,7 +135,7 @@ export default function Records() {
 //                 <Spinner animation="border" variant="primary" />
 //             </>
 //         ): ""}
-        
+
 //         {content.map((user) => {
 //           return (
 //             <ListGroup as="ol" key={user.id} horizontal={user.id}>
@@ -119,12 +145,12 @@ export default function Records() {
 //               >
 //                 <div className="ms-2 me-auto">
 //                   <div className="fw-bold">{user.name}</div>
-//                   {user.company.name}, {user.email} 
+//                   {user.company.name}, {user.email}
 //                 </div>
 //                 <Badge bg="primary" pill>
 //                   clock-in
 //                 </Badge>
-//               </ListGroup.Item>              
+//               </ListGroup.Item>
 //             </ListGroup>
 //           );
 //         })}
